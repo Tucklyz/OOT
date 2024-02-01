d4c733d3cbd5786e64e7ee6f00ef3bdb1c840083
Pung pond <154989924+Tucklyz@users.noreply.github.com>
2024-02-01 20:44:57 +0700

    Delete week10 directory

--- week10/lab/lab1/UserToolBox.java
+++
@@ -1,7 +0,0 @@
-package week10.lab.lab1;
-
-public interface UserToolBox extends ToolBox{
-    public abstract void setNamesOfUser(String fullName);
-    public abstract void getInfo();
-    
-}
--- week10/lab/lab2/ToolBox.java
+++
@@ -1,5 +0,0 @@
-package week10.lab.lab2;
-
-public interface ToolBox{
-    public abstract void getInfo();
-}
